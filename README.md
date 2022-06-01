b8fb0cdb7c07107e44af920a8b1a24bca98505f5
Avik Roy <avik3.1416@gmail.com>
2022-05-31 19:22:56 -0500

    somethings new

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 This is my own git repo to play with github CI.
+Making some changes now.
--- filetester.py
+++ filetester.py
@@ -1,16 +1,24 @@
 import sys, os
 
+f_chkd = "CheckedFiles.txt"
+this_f = open(f_chkd, "w")
+
 _files = os.listdir('files')
 checks_passed = True
+
 for _file in _files:
     f = open('files/' + _file, "r")
     l = f.readlines()
     if len(l) == 1:
         print("Checks passed for file: " + _file)
+        this_f.write(_file + '\n')
         f.close()
     else:
         print("Checks failed for file: " + _file)
         f.close()
         checks_passed = False
+
+this_f.close()
+
 if not checks_passed:
     sys.exit(1)
